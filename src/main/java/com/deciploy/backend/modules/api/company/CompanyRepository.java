9b5be25b822578b227045e671c32585c7c4f87af
imasshaWe <imasha98.we@gmail.com>
2023-09-30 15:14:04 +0530

    Updated repo

--- src/main/java/com/deciploy/backend/modules/api/company/CompanyRepository.java
+++ src/main/java/com/deciploy/backend/modules/api/company/CompanyRepository.java
@@ -3,7 +3,5 @@ package com.deciploy.backend.modules.api.company;
 import com.deciploy.backend.modules.api.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
+public interface CompanyRepository extends JpaRepository<Company, String> {
 }
