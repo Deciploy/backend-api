ff33e2d16c7a07f0322cf0918f03dbd386b462cb
imasshaWe <imasha98.we@gmail.com>
2023-09-30 15:14:35 +0530

    Added companyId and teamId

---
+++ src/main/java/com/deciploy/backend/modules/api/user/dto/CreateUserRequest.java
@@ -0,0 +1,21 @@
+package com.deciploy.backend.modules.api.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequest(
+        @NotBlank() String fullName,
+
+        @NotBlank() @Email() String email,
+
+        @NotBlank() String password,
+
+        @NotEmpty() @Size(min = 1) String[] roles,
+
+        String companyId,
+
+        String teamId
+) {
+}
