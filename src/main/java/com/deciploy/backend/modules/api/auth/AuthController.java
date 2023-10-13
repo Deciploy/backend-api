e4786a386545d59a559c731d30c09d674c55f191
imasshaWe <imasha98.we@gmail.com>
2023-10-13 22:55:55 +0530

    Added role based access level

--- src/main/java/com/deciploy/backend/modules/api/auth/AuthController.java
+++ src/main/java/com/deciploy/backend/modules/api/auth/AuthController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Secured("ADMIN")
     @PostMapping("/register")
     public ResponseEntity register(@Valid @RequestBody RegisterRequest registerRequest) {
         authService.register(registerRequest);
