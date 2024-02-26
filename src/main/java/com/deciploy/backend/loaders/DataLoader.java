70bab942458f12034e433cf53d1db066961a6cdc
imasshaWe <imasha98.we@gmail.com>
2024-02-26 20:01:35 +0530

    Implemented superuser on load

---
+++ src/main/java/com/deciploy/backend/loaders/DataLoader.java
@@ -0,0 +1,39 @@
+package com.deciploy.backend.loaders;
+
+import com.deciploy.backend.modules.api.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+    @Value("${security.super-user.email}")
+    String superUserEmail;
+    @Value("${security.super-user.password}")
+    String superUserPassword;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        loadUsers();
+    }
+
+    private void loadUsers() {
+        if (userService.getUserByEmail(superUserEmail) != null) {
+            return;
+        }
+
+        userService.saveUser(
+                "Super Admin",
+                superUserEmail,
+                passwordEncoder.encode(superUserPassword),
+                new String[]{"ADMIN"}
+        );
+    }
+}
